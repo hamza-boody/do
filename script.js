17bbe85632671cdcb87e58ac526deff06970832e
hamza-boody <boodyhamzo@gmail.com>
2025-03-14 10:00:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let a=prompt("what fruit and vegtbles you want to buy ?")
+switch(a){
+    case "banana":
+        document.write("15 EGP 1 kg.")
+        break;
+        case "apple":
+            document.write("50 EGP 1 kg.")
+            break;
+            case "orange":
+                document.write("20 EGP 1 kg.")
+                break;
+                case "strawbery":
+                    document.write("20 EGP 1 kg.")
+                    break;
+                    case  "water melon":
+                        document.write("20 EGP 1 kg.")
+                        break;
+                        case  "mango":
+                            document.write("25 EGP 1 kg.")
+                    
+                break;
+
+                    case  "cuocumber":
+                        document.write("20 EGP 1 kg.")
+                        break;
+                        case  "tomato":
+                            document.write("30 EGP 1 kg.")
+                            case "lettuce":
+                                document.write("20 EGP 1 kg.")
+        
+    
+}
